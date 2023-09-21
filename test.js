fa9383787b141e474ee0bcc6bbe8a205edec4930
Moh7ven <Mohamedsangare0107@gmail.com>
2023-09-21 13:32:04 +0000

    function mostWord

---
+++ test.js
@@ -0,0 +1,21 @@
+/* 
+Creer une fonction mostWord qui prend en paramètre une chaine de caractère 
+et retourne le mot le long de cette chaine. 
+*/
+
+function mostWord(chaine) {
+  if (typeof chaine === "string") {
+    let decoupeWord = chaine.split(" ");
+    let taille = [];
+    console.log(decoupeWord);
+
+    decoupeWord.map((e) => {
+      taille.push(e.length);
+    });
+    console.log(Math.max(...taille));
+  }else {
+    console.log("Veuiller entrer une chaine de caractère s'il vous pla");
+  }
+}
+
+mostWord("Bonjour le monde !");
